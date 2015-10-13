afce4e31825fce185e404b44d1df671a8600eb24
olaande <olaande@users.noreply.github.com>
2015-10-13 18:38:08 +0200

    Big update
    Jadå.

--- src/LightingBolt.java
+++ src/LightingBolt.java
@@ -6,36 +6,72 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.view.View;
 
 /**
- * Created by Ola on 2015-10-02.
- * Version 0.2
+ * Spawns a lightingbolt on a specific lane
+ *
+ * @author 		Ola Andersson
+ * @version		0.3
+ * @since		2015-09-29
  */
 public class LightingBolt {
 
     private LightingBoltView view;
 
-    public final int left;
-    public int top = 1200;
-    public final int right;
-    public int bottom = 1400;
+    private final double left;
+    private double top;
+    private final double right;
+    public double bottom;
 
-    public LightingBolt(int lane){
-        left = (200*lane)+40;
-        right = (200*lane)+240;
+    private Rect lightingRect;
+
+    private final double LEFT_SCALE_FACTOR = 0.176592;
+    private final double TOP_SCALE_FACTOR = 0.6510417;
+    private final double RIGHT_SCALE_FACTOR = 0.242592;
+    private final double BOTTOM_SCALE_FACTOR = 0.703125;
+    private final double UPDATE_RATIO = 20;
+
+    /**
+     *Initializes a lightingbolt animation.
+     */
+
+    public LightingBolt(){
+        int lane = Bus.lane-1;
+        int width = Bus.width;
+        int height = Bus.height;
+        double moveScaleFactor = Bus.MOVE_SCALE_FACTOR;
+
+        left = ((width * moveScaleFactor) * lane) + (width * LEFT_SCALE_FACTOR);
+        top = height * TOP_SCALE_FACTOR;
+        right = ((width * moveScaleFactor) * lane) + (width * RIGHT_SCALE_FACTOR);
+        bottom = height * BOTTOM_SCALE_FACTOR;
+        lightingRect = new Rect((int)left,(int)top,(int)right,(int)bottom);
     }
 
+    /**
+     * Sets the giving view to gone.
+     * @param lbv - make the param lbv gone.
+     */
     public void gone(View lbv){
         lbv.setVisibility(View.GONE);
     }
 
+    /**
+     * Public updater for the animation.
+     */
     public void update() {
-        top = top - 20;
-        bottom = bottom - 20;
+        top = top - UPDATE_RATIO;
+        bottom = bottom - UPDATE_RATIO;
         view.invalidate();
     }
 
+    /**
+     *
+     * @param activity - current activity to spawn a new lightingbolt.
+     * @return - a new instance of a lightigbolt.
+     */
     public View getView(Activity activity){
         if(view == null){
             view = new LightingBoltView(activity);
@@ -43,18 +79,34 @@ public class LightingBolt {
         return view;
     }
 
+    /**
+     * Public method to get a Rect.
+     *
+     * @return - a lightingRect.
+     */
     public Rect getRect(){
-        return view.lightingRect;
+        return lightingRect;
     }
 
+    /**
+     * Inner class for drawing lightingbolts.
+     *
+     * @author Ola Andersson
+     * @version 0.3
+     * @since 2015-09-29
+     */
     private class LightingBoltView extends View {
 
-        private Rect lightingRect = new Rect(left,top,right,bottom);
-        private Paint paint = new Paint();
+        private Drawable drawableLB;
 
+        /**
+         * Initializes a lightingbolt on the background.
+         *
+         * @param context - current context to render in.
+         */
         public LightingBoltView(Context context){
             super(context);
-            paint.setColor(Color.YELLOW);
+            drawableLB = getResources().getDrawable(R.drawable.lighting);
         }
 
         @Override
@@ -62,11 +114,16 @@ public class LightingBolt {
             super.onSizeChanged(w, h, oldw, oldh);
         }
 
+        /**
+         * Draws the lightingbolt with the given params.
+         * @param canvas
+         */
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
-            lightingRect.set(left, top, right, bottom);
-            canvas.drawRect(lightingRect,paint);
+            lightingRect.set((int)left,(int)top,(int)right,(int)bottom);
+            drawableLB.setBounds(lightingRect);
+            drawableLB.draw(canvas);
         }
     }
 }
