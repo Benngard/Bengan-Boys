713484adddf59b4a92f226796caa7d057ba1d59d
antonandren <kruci@live.se>
2015-10-18 16:44:03 +0200

    ObstacleSpawner
    Simple obstacleSpawner that dispatches objects that spawn on the top of
    the screen into the playfield.

---
+++ src/ObstacleSpawner.java
@@ -0,0 +1,57 @@
+package com.example.hyperion;
+
+
+/**
+ * @author Anton Andrén
+ * @version 0.7
+ * @since 2015-10-15
+ *
+ * ObstacleSpawner is responsible for spawning all the objects spawning on the top of the screen.
+ */
+
+public class ObstacleSpawner {
+
+    Obstacles obstacles;
+    BussReader bussReader;
+    int frequencyTimer = 0;
+
+    /**
+     * Constructor for the ObstacleSpawner, saves a reference of Onstacles and bussReader
+     * @param obstacles
+     * @param bussReader
+     */
+    public ObstacleSpawner (Obstacles obstacles, BussReader bussReader) {
+        this.obstacles = obstacles;
+        this.bussReader = bussReader;
+    }
+
+    /**
+     * Spawns objects on the top of the screen based on the eventQueue in bussReader and also spawns energy refills.
+     */
+    public void spawnObstacle () {
+        frequencyTimer++;
+        if (frequencyTimer % 15 == 0) {
+            switch (bussReader.getEvent()) {
+                case DOOR: {
+                    obstacles.spawnFenceLeft();
+                    obstacles.spawnFenceRight();
+                    obstacles.spawnPower(3);
+                }
+                case INDICATOR: {
+                    obstacles.spawnRockLeft();
+                    obstacles.spawnPower(5);
+                }
+                case STOP: {
+                    obstacles.spawnRockRight();
+                    obstacles.spawnPower(1);
+                }
+                case EMPTY: {
+                    obstacles.spawnPower(2);
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+    }
+}
