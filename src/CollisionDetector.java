99908008d29b097635d271fc83416b7ca5c3603b
antonandren <kruci@live.se>
2015-10-18 15:16:55 +0200

    Small update
    Now removes lightingbolts and energy upon collision.

--- src/CollisionDetector.java
+++ src/CollisionDetector.java
@@ -11,32 +11,39 @@ import java.util.List;
 /**
  * @author Anton Andrén
  * @since 2015-10-10
- * @version  0.5
+ * @version  0.6
  *
- * Class for detecting and handeling collisions between objects in Playfield.
+ * Class for detecting and handling collisions between objects in Playfield.
  */
 public class CollisionDetector {
 
+    /**
+     * CheckCollisions checks for collisions between objects in the PlayField and handles them
+     * @param lightingBolts - List of all the LightinBolt objects in playField
+     * @param bus           - The bus object in PlayField
+     * @param obstacles     - Contains lists of all the invulnerable and Collectible objects in the PlayField
+     */
     public void checkCollsion (List<LightingBolt>lightingBolts, Bus bus, Obstacles obstacles) {
         Rect busRect = bus.getRect();
 
         for (LightingBolt lb : lightingBolts){
             for (Invulnerable inv : obstacles.getInvulnerables()){
                 if(lb.getRect().intersect(inv.getRect())){
-                    // despawn lightning
+                    lightingBolts.remove(lb);
                 }
             }
         }
 
         for (Invulnerable invulnerable : obstacles.getInvulnerables()){
             if(busRect.intersect(invulnerable.getRect())){
-                // Trigger game over.
+                // run game over
             }
         }
 
         for (Collectible collectible : obstacles.getCollectibles()){
             if(busRect.intersect(collectible.getRect())){
                 bus.getPowerComponent().addPower();
+                obstacles.getCollectibles().remove(collectible);
             }
         }
     }
